aba17d240f866e4ae5d4eaafd8da0a5ab0e07767
qwe-qwe1 <1787821427@qq.com>
2021-09-03 18:13:20 +0800

    router 0903

--- src/13_learn_vue_router/App.vue
+++ src/13_learn_vue_router/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div>
-    <router-link to="/home">首页</router-link>
+    <router-link to="/home" tag=""> <nav-bar title="首页"/></router-link>
     <router-link to="/about">关于</router-link>
     <router-link to="/user/aaa/id/123">用户</router-link>
     <button @click="jumpToAbout">关于</button>
-    <router-view></router-view>
+
+    <transition>
+      <component :is="组件"></component>
+    </transition>
+
+    <router-view v-slot="props">
+      <transition name="why">
+        <keep-alive>
+          <component :is="props.Component"></component>
+        </keep-alive>
+      </transition>
+    </router-view>
   </div>
 </template>
 
 <script>
 import { useRouter } from 'vue-router'
+import NavBar from './components/NavBar.vue'
 export default {
   setup() {
     const router = useRouter()
@@ -25,8 +37,24 @@ export default {
     return {
       jumpToAbout
     }
+  },
+  components: {
+    NavBar
   }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.why-active {
+  color: red;
+}
+.why-enter-from,
+.why-leave-to {
+  opacity: 0;
+}
+
+.why-enter-active,
+.why-leave-active {
+  transition: opacity 1s ease;
+}
+</style>
